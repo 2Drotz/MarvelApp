7db64c621040b02c299fce90bb1a547f5ddb1771
Dmitry <mandrin2013@mail.ru>
2022-10-27 17:48:11 +0300

    commit

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,32 @@
 import AppHeader from "../appHeader/AppHeader";
+import { Component } from 'react';
 import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
+import CharList from "../charList/charList";
 import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+class App extends Component {
+
+    state = {
+        selectedChar: null
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <RandomChar />
+                    <div className="char__content">
+                        <CharList />
+                        <CharInfo />
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision" />
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,7 +1,7 @@
 class MarvelService {
    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
    // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
-   _apiKey = 'apikey=5b54fd1bfcb47c5d1972bed5a13df785';
+   _apiKey = 'apikey=c5d6fc8b83116d92ed468ce36bac6c62';
 
    getResource = async (url) => {
       let res = await fetch(url);
@@ -14,8 +14,8 @@ class MarvelService {
    }
 
    getAllCharacters = async () => {
-      const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=50&${this._apiKey}`);
-      return res.data.result.map(this._transformCharacter);
+      const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+      return res.data.results.map(this._transformCharacter);
    }
 
    getCharacter = async (id) => {
@@ -23,15 +23,15 @@ class MarvelService {
       return this._transformCharacter(res.data.results[0]);
    }
 
-
    _transformCharacter = (char) => {
       return {
          id: char.id,
          name: char.name,
-         description: char.description ? `${char.description.substr(0, 210)}...` : "Извините, данные по этому персонажу отсутствуют",
+         description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
          thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
          homepage: char.urls[0].url,
-         wiki: char.urls[1].url
+         wiki: char.urls[1].url,
+         comics: char.comics.items
       }
    }
 }
