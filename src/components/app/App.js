e2f7d94dad6a674b367f8a9f717c7252f605daf9
Dmitry <mandrin2013@mail.ru>
2022-11-14 16:35:57 +0300

    router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/charList";
 import CharInfo from "../charInfo/CharInfo";
+import ComicsList from '../comicsList/ComicsList';
+import AppBanner from "../appBanner/AppBanner";
 import ErrorBoundery from '../errorBoundery/ErrorBoundery'
 import decoration from '../../resources/img/vision.png';
 
@@ -17,23 +21,34 @@ const App = () => {
     }
 
     return (
-        <div className="app">
-            <AppHeader />
-            <main>
-                <ErrorBoundery>
-                    <RandomChar />
-                </ErrorBoundery>
-                <div className="char__content">
-                    <ErrorBoundery>
-                        <CharList onCharSelected={onCharSelected} />
-                    </ErrorBoundery>
-                    <ErrorBoundery>
-                        <CharInfo charId={selectedChar} />
-                    </ErrorBoundery>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision" />
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader />
+                <main>
+                    <Switch>
+                        <Route exact path='/'>
+                            <ErrorBoundery>
+                                <RandomChar />
+                            </ErrorBoundery>
+                            <div className="char__content">
+                                <ErrorBoundery>
+                                    <CharList onCharSelected={onCharSelected} />
+                                </ErrorBoundery>
+                                <ErrorBoundery>
+                                    <CharInfo charId={selectedChar} />
+                                </ErrorBoundery>
+                            </div>
+                            <img className="bg-decoration" src={decoration} alt="vision" />
+                        </Route>
+                        <Route exact path='/comics'>
+                            <AppBanner />
+                            <ComicsList />
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
+
     )
 
 }
