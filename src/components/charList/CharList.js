813d10e5b65c2c66c5c56c4819baaf721e8fa6d9
Dmitry <mandrin2013@mail.ru>
2022-11-29 14:00:16 +0300

    update page app. js route with element to page

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 
 import PropTypes from 'prop-types';
-import Spinner from '../spiner/spiner';
+import Spinner from '../spinner/Spinner';
 
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import useMarvelService from '../../services/MarvelService';
